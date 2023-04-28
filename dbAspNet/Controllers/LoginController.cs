99f173eafae97a67d78915041a06368e000dd842
KieuMei2811 <hothikieu28112003@gmail.com>
2023-04-28 10:31:10 +0700

    C#

---
+++ dbAspNet/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using dbAspNet.Models;
+using dbAspNet.Code;
+
+namespace dbAspNet.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModel model)
+        {
+            var result = new accountModel().Login(model.UserName,
+            model.Password);
+            if (result && ModelState.IsValid)
+            {
+                SessionHelper.SetSession(new UserSession()
+                {
+                    UserName =
+                model.UserName
+                });
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Tên đăng nhập hoặc mật khẩu không đúng");
+            }
+            return View(model);
+        }
+    }
+}
